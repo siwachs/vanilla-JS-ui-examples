a364af3ef3d8ea6b9daa70a4a822175be160f4f8
siwachs <ssiwach.8888@gmail.com>
2024-03-07 20:27:40 +0530

    Init Infinite Carousel Circular

---
+++ Infinite Carousel slider (circular)/script.js
@@ -0,0 +1,7 @@
+const slider = document.querySelector(".slider");
+const prev = document.querySelector(".arrow-prev");
+const next = document.querySelector(".arrow-next");
+
+next.addEventListener("click", () => {});
+
+prev.addEventListener("click", () => {});
