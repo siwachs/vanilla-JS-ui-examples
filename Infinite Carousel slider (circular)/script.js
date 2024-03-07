2635449b760b3ca1b64b4636c8ebd90aba6bcf16
siwachs <ssiwach.8888@gmail.com>
2024-03-07 21:35:55 +0530

    Infinite Slider

--- Infinite Carousel slider (circular)/script.js
+++ Infinite Carousel slider (circular)/script.js
@@ -1,7 +1,34 @@
 const slider = document.querySelector(".slider");
+const carousel = document.querySelector(".carousel");
 const prev = document.querySelector(".arrow-prev");
 const next = document.querySelector(".arrow-next");
+let direction;
 
-next.addEventListener("click", () => {});
+next.addEventListener("click", () => {
+  direction = -1;
+  carousel.style.justifyContent = "flex-start";
+  slider.style.transform = "translate(-20%)";
+});
 
-prev.addEventListener("click", () => {});
+slider.addEventListener("transitionend", () => {
+  if (direction === -1) {
+    slider.appendChild(slider.firstElementChild);
+  } else {
+    slider.prepend(slider.lastElementChild);
+  }
+
+  slider.style.transition = "none";
+  slider.style.transform = "translate(0)";
+  setTimeout(() => {
+    slider.style.transition = "all 0.5s";
+  });
+});
+
+prev.addEventListener("click", () => {
+  if (direction === -1) {
+    slider.appendChild(slider.firstElementChild);
+    direction = 1;
+  }
+  carousel.style.justifyContent = "flex-end";
+  slider.style.transform = "translate(20%)";
+});
