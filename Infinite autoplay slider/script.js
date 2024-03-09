c9cca5d37bc60047f1b60adb1d28c6156a509f80
siwachs <ssiwach.8888@gmail.com>
2024-03-09 10:29:16 +0530

    Infinite Slides with slide clone

--- Infinite autoplay slider/script.js
+++ Infinite autoplay slider/script.js
@@ -2,8 +2,12 @@ const slideContainer = document.querySelector(".container");
 const slidesContainer = document.querySelector(".container .slides");
 const prevBtn = document.getElementById("slide-prev-btn");
 const nextBtn = document.getElementById("slide-next-btn");
+let sliderInterval;
+const interval = 3600;
 
-let slides = document.querySelectorAll(".container .slides .slide");
+const getSlides = () => document.querySelectorAll(".container .slides .slide");
+
+let slides = getSlides();
 let index = 1;
 
 let firstSlideClone = slides[0].cloneNode(true); // Deep copy clone child too
@@ -14,3 +18,51 @@ lastSlideClone.id = "last-slide-clone";
 
 slidesContainer.append(firstSlideClone);
 slidesContainer.prepend(lastSlideClone);
+slides = getSlides();
+
+const slideWidth = slides[index].clientWidth;
+slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+
+const autoPlaySlides = () => {
+  sliderInterval = setInterval(() => {
+    index++;
+    slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+    slidesContainer.style.transition = "transform 0.3s";
+  }, interval);
+};
+
+slidesContainer.addEventListener("transitionend", () => {
+  slides = getSlides();
+  if (slides[index]?.id === firstSlideClone.id) {
+    slidesContainer.style.transition = "none";
+    index = 1;
+    slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+
+  if (slides[index]?.id === lastSlideClone.id) {
+    slidesContainer.style.transition = "none";
+    index = slides.length - 2;
+    slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+});
+
+slidesContainer.addEventListener("mouseenter", () => {
+  clearInterval(sliderInterval);
+});
+slideContainer.addEventListener("mouseleave", autoPlaySlides);
+autoPlaySlides();
+
+nextBtn.addEventListener("click", () => {
+  slides = getSlides();
+  if (index >= slides.length - 1) return;
+  index++;
+  slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+  slidesContainer.style.transition = "transform 0.3s";
+});
+
+prevBtn.addEventListener("click", () => {
+  if (index <= 0) return;
+  index--;
+  slidesContainer.style.transform = `translateX(${-slideWidth * index}px)`;
+  slidesContainer.style.transition = "transform 0.3s";
+});
