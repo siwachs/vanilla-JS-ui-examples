0292aaf07b52760fdf8a9281311811506218ee49
siwachs <ssiwach.8888@gmail.com>
2024-03-13 08:40:45 +0530

    Fix Animation JS Bug

--- Animated Slider For Starbucks Coffee/script.js
+++ Animated Slider For Starbucks Coffee/script.js
@@ -7,40 +7,6 @@ let active = 1;
 let other_1 = null;
 let other_2 = null;
 
-const changeSlide = () => {
-  const prevActiveSlide = document.querySelector(
-    ".carousel .list .item.active"
-  );
-  const other_1 = document.querySelector(".carousel .list .item.other_1");
-  const other_2 = document.querySelector(".carousel .list .item.other_2");
-
-  if (prevActiveSlide) prevActiveSlide.classList.remove("active");
-  if (other_1) other_1.classList.remove("other_1");
-  if (other_2) other_2.classList.remove("other_2");
-
-  // Refresh Animation
-  items.forEach((item) => {
-    item.querySelector(".image img").style.animation = "none";
-    item.querySelector(".image figcaption").style.animation = "none";
-    item.offsetWidth; // Trigger reflow
-    item.querySelector(".image img").style.animation = "";
-    item.querySelector(".image figcaption").style.animation = "";
-  });
-
-  items[active].classList.add("active");
-  items[other_1].classList.add("other_1");
-  items[other_2].classList.add("other_2");
-  clearInterval(autoplay);
-  autoplay = setInterval(() => {
-    next.click();
-  }, 5000);
-};
-
-// Autoplay Slider
-let autoplay = setInterval(() => {
-  next.click();
-}, 5000);
-
 next.onclick = () => {
   carousel.classList.remove("prev");
   carousel.classList.add("next");
@@ -57,4 +23,29 @@ prev.onclick = () => {
   other_1 = active + 1 > totalItems ? 0 : active + 1;
   other_2 = active + 1 > totalItems ? 0 : active + 1;
   changeSlide();
+};
+
+const changeSlide = () => {
+  const prevActiveSlide = document.querySelector(
+    ".carousel .list .item.active"
+  );
+  if (prevActiveSlide) prevActiveSlide.classList.remove("active");
+
+  const other_1Slide = document.querySelector(".carousel .list .item.other_1");
+  if (other_1Slide) other_1Slide.classList.remove("other_1");
+
+  const other_2Slide = document.querySelector(".carousel .list .item.other_2");
+  if (other_2Slide) other_2Slide.classList.remove("other_2");
+
+  items.forEach((item) => {
+    item.querySelector(".image img").style.animation = "none";
+    item.querySelector(".image figcaption").style.animation = "none";
+    const refreshAnimation = item.offsetWidth;
+    item.querySelector(".image img").style.animation = "";
+    item.querySelector(".image figcaption").style.animation = "";
+  });
+
+  items[active].classList.add("active");
+  items[other_1].classList.add("other_1");
+  items[other_2].classList.add("other_2");
 };
