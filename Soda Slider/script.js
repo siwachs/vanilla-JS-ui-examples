f733e670550c15b0f32b207e5bcceac68505658f
siwachs <ssiwach.8888@gmail.com>
2024-03-18 12:44:51 +0530

    360 Degree Slider Script.js

--- Soda Slider/script.js
+++ Soda Slider/script.js
@@ -8,6 +8,11 @@ let lastItemPostition = listItems.length - 1;
 let active = 0;
 let zIndexOfActiveItem = 2;
 
+let removeEffect;
+let autoRun = setInterval(() => {
+  next.click();
+}, 5000);
+
 next.onclick = () => {
   const newActive = active + 1 > lastItemPostition ? 0 : active + 1;
   setActiveItem(newActive, showSlider);
@@ -25,15 +30,20 @@ const setActiveItem = (newActive, showSlider) => {
   showSlider(actionType);
 };
 
-let removeEffect;
-
 const showSlider = (actionType) => {
-  const oldActiveItem = document.querySelector(".carousel .list .item");
+  carousel.style.pointerEvents = "none";
+  const oldActiveItem = document.querySelector(".carousel .list .item.active");
   if (oldActiveItem) {
     oldActiveItem.classList.remove("active");
     zIndexOfActiveItem++;
     listItems[active].style.zIndex = zIndexOfActiveItem;
     listItems[active].classList.add("active");
+  }
+
+  if (actionType === "next") {
+    carousel.style.setProperty("--transform", "300px");
+  } else {
+    carousel.style.setProperty("--transform", "-300px");
   }
 
   // Add a animation while switching slides
@@ -48,5 +58,17 @@ const showSlider = (actionType) => {
   clearTimeout(removeEffect);
   removeEffect = setTimeout(() => {
     carousel.classList.remove("effect");
+    carousel.style.pointerEvents = "auto";
   }, 1500);
+
+  clearTimeout(autoRun);
+  autoRun = setTimeout(() => {
+    next.click();
+  }, 5000);
 };
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    setActiveItem(index, showSlider);
+  });
+});
